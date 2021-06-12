73f933e4d696ff77f1c8da664ec0f9bc96cd17ae
ChaseTheCoder <chasefsheaff@gmail.com>
2021-06-12 13:41:57 -0500

    feat: command line arguments accepted

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("hello world")
+// import books from 'google-books-search';
+
+var myArgs = process.argv.slice(2);
+console.log('myArgs: ', myArgs);
+
+// books.search(search, function(error, results) {
+//   if ( ! error ) {
+//       console.log(results);
+//   } else {
+//       console.log(error);
+//   }
+// });
