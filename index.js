31783059dd8b165449b439b6430b8a034e885c9a
ChaseTheCoder <chasefsheaff@gmail.com>
2021-06-13 11:46:23 -0500

    feat: inpuirer search and print book list

--- index.js
+++ index.js
@@ -1,15 +1,5 @@
 import books from 'google-books-search';
-
-
-
-let myArgs = process.argv.slice(2);
-console.log('myArgs: ', myArgs);
-
-let query = "";
-
-for(let i = 0; i < myArgs.length; i++){
-  query += myArgs[i] + " ";
-}
+import inquirer from 'inquirer';
 
 class Book {
   constructor(title, authors, publisher){
@@ -52,11 +42,11 @@ class BookList {
     let current = this.head;
     let counter = 1;
     while(counter < 6){
-      console.log("Book " + counter + " -----------------");
-      console.log("Title: " + current.title);
-      console.log("Authors: " + current.authors);
-      console.log("Publisher: " + current.publisher);
-      console.log(" ");
+      console.log('Book ' + counter + ' -----------------');
+      console.log('Title: ' + current.title);
+      console.log('Authors: ' + current.authors);
+      console.log('Publisher: ' + current.publisher);
+      console.log(' ');
       current = current.next;
       counter++;
     }
@@ -66,13 +56,79 @@ class BookList {
 let querySearchResults = new BookList();
 let readingList = new BookList();
 
-books.search(query, function(error, results) {
-  if ( ! error ) {
-      for(let i = 0; i < 5; i++){
-          querySearchResults.push(results[i].title, results[i].authors, results[i].publisher)
-      }
-      querySearchResults.print();
-  } else {
-      console.log(error);
+function bookQuery(query) {
+  books.search(query, function(error, results) {
+    if ( ! error ) {
+        for(let i = 0; i < 5; i++){
+            querySearchResults.push(results[i].title, results[i].authors, results[i].publisher)
+        }
+        querySearchResults.print();
+    } else {
+        console.log(error);
+    }
+  });
+}
+
+'use strict';
+console.log('Hello, search books via Google and add them to a Reading List.');
+
+const question1 = [
+  {
+    type: 'input',
+    name: 'bookSearch',
+    message: 'Search book: ',
+  },
+];
+const question2 = [
+  {
+    type: 'list',
+    name: 'chooseBook',
+    message: 'Which book do you want to add to your reading list? Enter the number: ',
+    choices: [1,2,3,4,5]
   }
-});
+];
+const question3 = [
+  {
+    type: 'confirm',
+    name: 'askAgain',
+    message: 'Hit enter to search again.',
+  }
+];
+
+function ask() {
+  inquirer
+  .prompt(
+    question1
+  )
+  .then((answer1) => {
+    bookQuery(answer1.bookSearch);
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      console.log(error.isTtyError)
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      querySearchResults.print();
+    }
+  });
+}  
+
+// function selectBook() {
+//   inquirer
+//   .prompt(question2)
+//   .then((answer2) => {
+//     let selection = querySearchResults.get(answer2)
+//     console.log(selection);
+//   })
+//   .catch((error) => {
+//     if (error.isTtyError) {
+//       console.log(error.isTtyError)
+//       // Prompt couldn't be rendered in the current environment
+//     } else {
+//       console.log("Good!");
+//     }
+//   });
+// }
+
+
+ask();
